f27a30686aa3974910ee962d2efd4352512aea00
mistu <Azahar SK>
2026-03-08 14:12:46 +0530

    add PracticeSet/

---
+++ PracticeSet/variables_dt.py
@@ -0,0 +1,57 @@
+#variables - use to store the data
+
+# # data types
+# v_str = "najrin"
+# v_int = 5
+# v_float = 9.0
+# v_bool = True
+# print(v_str + " " + str(v_int) + " "+ str(v_bool))
+# print(f"{v_str} {v_int} and {v_bool}") # formated string
+
+# My fathers age is 50 and My mothers age is 40 and my age is 10
+
+# v_fathers_age = 50
+# v_mothers_age = 40
+# v_my_age = v_fathers_age - v_mothers_age
+# print(f"My fathers age is {v_fathers_age} and My mothers age is {v_mothers_age} and my age is  {v_my_age}")
+
+# list variable - it is ordered set where you can store multiple  data types values
+
+# lst_1 = [1,2,3,4,5]
+
+# print(lst_1)
+
+# print(lst_1[1:3])
+
+# lst_1[0] = 10
+# lst_1[1] = 20
+# lst_1[2] = 30
+
+# print(lst_1)
+
+# tup_1 = (1,2,3,4,5)
+
+# print(tup_1)
+
+# print(tup_1[1:3])
+
+# tup_1[1] = 20
+
+# # dictionary - it stores dta in key value pairs
+
+# dic_1 = {"name": "najrin", "age": 26, "location": "kolkata"}
+# print(dic_1)
+# print(dic_1["age"])
+# print(dic_1.keys())
+# print(dic_1.values())
+# print(dic_1.items())
+
+# set - unique unordered list of items
+
+set_1 = {1,2,3,3,3,3,4,5,6}
+set_2 = {5,7,8,9}
+print(set_1)
+
+print(set_1.union(set_2))
+print(set_1.intersection(set_2))
+print(set_1.symmetric_difference(set_2))
